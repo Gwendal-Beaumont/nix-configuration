ab92c5d44368d71b4d5ebfe8f6fca8e014ae6606
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-06-19 16:35:23 +0200

    Change hardcoded username for variable

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
 	  {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
-	    home-manager.users.g23beaum = ./home/common;
+	    home-manager.users.${username} = ./home/common;
 
 	    # Optionally, use home-manager.extraSpecialArgs to pass
 	    # arguments to home.nix

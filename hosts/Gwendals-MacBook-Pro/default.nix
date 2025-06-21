c331645d48a6e23ce871c463b423adaf161d72c1
Gwendal Beaumont <gwendal.beaumont@gmail.com>
2025-06-21 09:48:40 +0200

    Small refactoring to macOS defaults

--- hosts/Gwendals-MacBook-Pro/default.nix
+++ hosts/Gwendals-MacBook-Pro/default.nix
@@ -1,8 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
-  users.users.gwendalbeaumont = {
-    name = "gwendalbeaumont";
-    home = "/Users/gwendalbeaumont";
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+    optimise.automatic = true;
+  };
+
+  users.users.${username} = {
+    name = "${username}";
+    home = "/Users/${username}";
   };
 
   # List packages installed in system profile. To search by name, run:
@@ -74,20 +87,13 @@
       "zotero"
     ];
   };
-
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
-
-  # Enable alternative shell support in nix-darwin.
-  # programs.fish.enable = true;
-
   # Set Git commit hash for darwin-version.
   # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
-  system.primaryUser = "gwendalbeaumont";
+  system.primaryUser = "${username}";
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";

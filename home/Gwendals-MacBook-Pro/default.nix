557fc4e3bbb29d19a41bb7638036e55004e001cd
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-06-23 11:00:25 +0200

    Import username on darwin

--- home/Gwendals-MacBook-Pro/default.nix
+++ home/Gwendals-MacBook-Pro/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 
 {
   imports = [
@@ -7,18 +7,19 @@
     ./git
   ];
 
-  home.username = "gwendalbeaumont";
+  home = {
+    username = "${username}";
 
-  home.packages = with pkgs; [
-    cairo
-    cocoapods
-    coreutils
-    nerd-fonts.jetbrains-mono
-  ];
+    packages = with pkgs; [
+      cairo
+      cocoapods
+      coreutils
+      nerd-fonts.jetbrains-mono
+    ];
 
-  home.stateVersion = "25.05";
+    stateVersion = "25.05";
+  };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   services.gpg-agent = {

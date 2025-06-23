5d9621eb0e7edd8ace6a9903ba790d2045596385
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-06-23 11:01:50 +0200

    Import username on NixOS

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, username, ... }:
 
 {
   imports = [
@@ -13,43 +13,34 @@
     ./kitty
   ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "g23beaum";
-  home.homeDirectory = "/home/g23beaum";
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
 
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-    # Utils
-    libimobiledevice
-    libsecret
-    usbutils
-    wl-clipboard
+    packages = with pkgs; [
+      # Utils
+      libimobiledevice
+      libsecret
+      usbutils
+      wl-clipboard
 
-    # Network
-    dnsutils
-    nmap
+      # Network
+      dnsutils
+      nmap
 
-    # Productivity
-    libreoffice-fresh
-    drawio
-    slack
+      # Productivity
+      libreoffice-fresh
+      drawio
+      slack
 
-    # Misc
-    which
-    pciutils
-    gnomeExtensions.appindicator
-  ];
+      # Misc
+      which
+      pciutils
+      gnomeExtensions.appindicator
+    ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "25.05";
+    stateVersion = "25.05";
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;

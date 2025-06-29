692c196407fb2f76d50ac58379867a2d989f19e0
Gwendal Beaumont <gwendal.beaumont@gmail.com>
2025-06-29 16:57:20 +0200

    Extract casks to a separate file

---
+++ hosts/Gwendals-MacBook-Pro/homebrew/casks.nix
@@ -0,0 +1,29 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  homebrew.casks = [
+      "android-studio"
+      "appcleaner"
+      "discord"
+      "drawio"
+      "eclipse-ide"
+      "element"
+      "firefox"
+      "google-chrome"
+      "kitty"
+      "libreoffice"
+      "logi-options+"
+      "openvpn-connect"
+      "protonvpn"
+      "signal"
+      "slack"
+      "thunderbird"
+      "visual-studio-code"
+      "vlc"
+      "webex"
+      "whatsapp"
+      "zoom"
+      "zotero"
+    ];
+
+}
--- hosts/Gwendals-MacBook-Pro/homebrew/default.nix
+++ hosts/Gwendals-MacBook-Pro/homebrew/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./casks.nix
     ./masApps.nix
   ];
 
@@ -12,31 +13,5 @@
       autoUpdate = true;
       cleanup = "uninstall";
     };
-
-    casks = [
-      "android-studio"
-      "appcleaner"
-      "discord"
-      "drawio"
-      "eclipse-ide"
-      "element"
-      "firefox"
-      "google-chrome"
-      "kitty"
-      "libreoffice"
-      "logi-options+"
-      "openvpn-connect"
-      "protonvpn"
-      "signal"
-      "slack"
-      "thunderbird"
-      "visual-paradigm-ce"
-      "visual-studio-code"
-      "vlc"
-      "webex"
-      "whatsapp"
-      "zoom"
-      "zotero"
-    ];
   };
 }

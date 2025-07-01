594e8833cb6770bbe06bf01ba3b055eb68b05253
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-07-01 14:18:24 +0200

    Activate GNOME module and remove unecessary activation

--- hosts/maple/default.nix
+++ hosts/maple/default.nix
@@ -1,9 +1,11 @@
-{ config, pkgs, hostname, username, ... }:
+{ config, pkgs, hostname, username, nixosModules, ... }:
 
 {
   imports = [
-      ./hardware-configuration.nix
-    ];
+    ./hardware-configuration.nix
+
+    "${nixosModules}/gnome"
+  ];
 
   # Bootloader.
   boot.loader = {
@@ -58,10 +60,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {

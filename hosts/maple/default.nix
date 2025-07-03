7bd6eb9995463de169c4030baff6867203fe5e6b
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-07-03 16:07:07 +0200

    Cleaner default.nix

--- hosts/maple/default.nix
+++ hosts/maple/default.nix
@@ -30,18 +30,6 @@
     networkmanager = {
       enable = true;
     };
-
-    # Configure network proxy if necessary
-    # proxy = {
-    #   default = "http://user:password@proxy:port/";
-    #   noProxy = "127.0.0.1,localhost,internal.domain";
-    # };
-
-    # Open ports in the firewall.
-    # firewall.allowedTCPPorts = [ ... ];
-    # firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # firewall.enable = false;
   };
 
   # Set your time zone.
@@ -84,27 +72,22 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
   # Garabge collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 1w";
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 1w";
+    };
+    settings.auto-optimise-store = true;
   };
 
-  nix.settings.auto-optimise-store = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User account. Don't forget to set a password with ‘passwd’.
   users.users.g23beaum = {
     isNormalUser = true;
     description = "Gwendal";

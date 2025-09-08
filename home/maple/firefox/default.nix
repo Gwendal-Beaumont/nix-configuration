773f18d464bd950be06fd7744ebe871a16d0dd72
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-09-08 11:21:29 +0200

    add Firefox policies on maple

--- home/maple/firefox/default.nix
+++ home/maple/firefox/default.nix
@@ -8,5 +8,36 @@
       "en-GB"
       "fr"
     ];
+
+    policies = {
+      "AutofillAdressEnabled" = false;
+      "AutofillCreditCardEnabled" = false;
+      "DisableFormHistory" = true;
+      "DisablePocket" = true;
+      "DisableProfileImport" = true;
+      "OfferToSaveLoginsDefault" = false;
+      "PasswordManagerEnabled" = false;
+      "SanitizeOnShutdown" = {
+        "Cache" = true;
+        "Cookies" = false;
+        "FormData" = true;
+        "History" = false;
+        "Sessions" = false;
+        "SiteSettings" = false;
+        "Locked" = false;
+      };
+      "SearchEngines" = {
+        "Add" = [
+          {
+            "Name" = "Nixpkgs (unstable)";
+            "URLTemplate" = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";
+            "Method" = "GET";
+            "IconURL" = "https://nixos.org/favicon.ico";
+            "Alias" = "@nix";
+            "Description" = "Search in nixpkgs (unstable)";
+          }
+        ];
+      };
+    };
   };
 }

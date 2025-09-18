00531a4096f83a86a14da3a6a87ecab147cf22c0
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-09-18 10:02:48 +0200

    remove AirPlay support for maple

--- hosts/maple/default.nix
+++ hosts/maple/default.nix
@@ -83,22 +83,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    # raopOpenFirewall = true;
-    # extraConfig.pipewire = {
-    #   "10-airplay" = {
-    #     "context.modules" = [
-    #       {
-    #         name = "libpipewire-module-raop-discover";
-
-    #         # increase the buffer size if you get dropouts/glitches
-    #         args = {
-    #           "raop.latency.ms" = 1500;
-    #         };
-    #       }
-    #     ];
-    #   };
-    # };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).

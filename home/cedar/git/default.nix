ee62887a0a9f35b3de77ff1e1a136ed34e1f4c29
Gwendal Beaumont <gwendal.beaumont@gmail.com>
2025-10-20 21:40:27 +0200

    update git settings
    This change happens as home-manager renamed some of the settings

--- home/cedar/git/default.nix
+++ home/cedar/git/default.nix
@@ -2,16 +2,16 @@
 
 {
   programs.git = {
-    settings.user.email = "gwendal.beaumont@gmail.com";
+    settings = {
+      user.email = "gwendal.beaumont@gmail.com";
 
-    signing = {
-      key = "847D7D2DB3796CB8789644E373692F251579041D";
-    };
-
-    extraConfig = {
       "includeIf \"gitdir:~/Documents/twindevops/\"" = {
         path = "~/.config/git/imt/.gitconfig";
       };
+    };
+
+    signing = {
+      key = "847D7D2DB3796CB8789644E373692F251579041D";
     };
   };
 }
